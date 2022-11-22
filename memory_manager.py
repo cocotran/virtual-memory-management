65825c4f696b9bbaf4f8f9df6feb23cef8b8c903
cocotran <anhquangtran2703@gmail.com>
2022-11-22 14:15:07 -0500

    Add lookup api

--- memory_manager.py
+++ memory_manager.py
@@ -1,8 +1,10 @@
+from cmath import inf
+
+
 class Page:
     def __init__(self, id=None, value=None) -> None:
         self.id = id
         self.value = value
-        self.last_access = None
 
     def __repr__(self) -> str:
         return f"{self.id} {self.value}"
@@ -21,22 +23,49 @@ class Page:
 class MainMemory:
     def __init__(self, page_num: int) -> None:
         self.pages = [Page() for i in range(page_num)]
-        self._empty_page_index = None
+        self._add_pointer = None
+        self._access_counter = 1
+        self._access_point = [None for _ in range(page_num)]
+
+    def get_access_point(self) -> int:
+        self._access_counter += 1
+        return self._access_counter
+
+    def update_add_pointer(self) -> None:
+        if self.has_empty_space():
+            return
+        least_access_point = inf
+        for i, page in enumerate(self._access_point):
+            if page < least_access_point:
+                self._add_pointer = i
 
     def has_empty_space(self) -> bool:
         for i, page in enumerate(self.pages):
             if page.is_empty():
-                self._empty_page_index = i
+                self._add_pointer = i
                 return True
         return False
 
     def add(self, id: str, value: str) -> None:
-        self.pages[self._empty_page_index] = Page(id, value)
+        if self._access_counter <= 3:  # still empty
+            self.update_add_pointer()
+        self.pages[self._add_pointer] = Page(id, value)
+        self._access_point[self._add_pointer] = self.get_access_point()
+        self.update_add_pointer()
 
     def delete(self, id: str) -> None:
         for i, page in enumerate(self.pages):
             if page.id == id:
                 self.pages[i] = None
+                self._access_point[i] = None
+
+    def get(self, id: str) -> Page:
+        for i, page in enumerate(self.pages):
+            if page.id == id:
+                self._access_point[i] = self.get_access_point()
+                self.update_add_pointer()
+                return page
+        return -1
 
 
 class Disk:
@@ -67,6 +96,15 @@ class Disk:
                     f.write(str(page) + "\n")
             f.close()
 
+    def get(self, id: str) -> Page:
+        with open(self.pages_file, "r") as f:
+            pages = self._get_pages([line.strip("\n") for line in f.readlines()])
+            f.close()
+            for page in pages:
+                if page.id == id:
+                    return page
+            return -1
+
 
 class MemoryManager:
     def __init__(self, page_num: int) -> None:
@@ -87,4 +125,10 @@ class MemoryManager:
         self.disk.delete(variable_id)
 
     def lookup(self, variable_id: str):
-        pass
+        var = self.main_memory.get(variable_id)
+        if var == -1:
+            var = self.disk.get(variable_id)
+            if var != -1:
+                self.disk.delete(variable_id)
+                self.main_memory.add(var.id, var.value)
+        return var
